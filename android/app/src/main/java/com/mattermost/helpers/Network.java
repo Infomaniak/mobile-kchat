e01dbefb0f25e1a0e72992b7721b14adca57651b
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2023-02-02 13:27:15 +0100

    Pass token in native requests

--- android/app/src/main/java/com/mattermost/helpers/Network.java
+++ android/app/src/main/java/com/mattermost/helpers/Network.java
@@ -5,11 +5,15 @@ import android.content.Context;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.ReadableMap;
 
 import com.mattermost.networkclient.APIClientModule;
 import com.mattermost.networkclient.enums.RetryTypes;
+
+import java.util.Iterator;
+import java.util.Map;
 
 import okhttp3.HttpUrl;
 import okhttp3.Response;
@@ -19,31 +23,112 @@ public class Network {
     private static APIClientModule clientModule;
     private static final WritableMap clientOptions = Arguments.createMap();
     private static final Promise emptyPromise = new ResolvePromise();
+    private static ReactApplicationContext reactContext;
 
     public static void init(Context context) {
-        final ReactApplicationContext reactContext = new ReactApplicationContext(context);
+        reactContext = new ReactApplicationContext(context);
         clientModule = new APIClientModule(reactContext);
         createClientOptions();
     }
 
+    public static WritableMap addHeaders(String baseUrl, ReadableMap options) {
+        WritableMap newOptions;
+        if (options == null) {
+            newOptions = Arguments.createMap();
+        } else {
+            newOptions = toWritableMap(options.toHashMap());
+        }
+        WritableMap headers = Arguments.createMap();
+
+        String token = Credentials.getCredentialsForServerSync(reactContext, baseUrl);
+        if (token != null) {
+            headers.putString("Authorization", "Bearer " + token);
+            headers.putString("Content-Type", "application/json");
+        }
+        newOptions.putMap("headers", headers);
+        return newOptions;
+    }
+
+    public static WritableMap toWritableMap(Map<String, Object> map) {
+        WritableMap writableMap = Arguments.createMap();
+        Iterator iterator = map.entrySet().iterator();
+
+        while (iterator.hasNext()) {
+            Map.Entry pair = (Map.Entry) iterator.next();
+            Object value = pair.getValue();
+
+            if (value == null) {
+                writableMap.putNull((String) pair.getKey());
+            } else if (value instanceof Boolean) {
+                writableMap.putBoolean((String) pair.getKey(), (Boolean) value);
+            } else if (value instanceof Double) {
+                writableMap.putDouble((String) pair.getKey(), (Double) value);
+            } else if (value instanceof Integer) {
+                writableMap.putInt((String) pair.getKey(), (Integer) value);
+            } else if (value instanceof String) {
+                writableMap.putString((String) pair.getKey(), (String) value);
+            } else if (value instanceof Map) {
+                writableMap.putMap((String) pair.getKey(), toWritableMap((Map<String, Object>) value));
+            } else if (value.getClass() != null && value.getClass().isArray()) {
+                writableMap.putArray((String) pair.getKey(), toWritableArray((Object[]) value));
+            }
+
+            iterator.remove();
+        }
+
+        return writableMap;
+    }
+
+    public static WritableArray toWritableArray(Object[] array) {
+        WritableArray writableArray = Arguments.createArray();
+
+        for (int i = 0; i < array.length; i++) {
+            Object value = array[i];
+
+            if (value == null) {
+                writableArray.pushNull();
+            }
+            if (value instanceof Boolean) {
+                writableArray.pushBoolean((Boolean) value);
+            }
+            if (value instanceof Double) {
+                writableArray.pushDouble((Double) value);
+            }
+            if (value instanceof Integer) {
+                writableArray.pushInt((Integer) value);
+            }
+            if (value instanceof String) {
+                writableArray.pushString((String) value);
+            }
+            if (value instanceof Map) {
+                writableArray.pushMap(toWritableMap((Map<String, Object>) value));
+            }
+            if (value.getClass().isArray()) {
+                writableArray.pushArray(toWritableArray((Object[]) value));
+            }
+        }
+
+        return writableArray;
+    }
+
     public static void get(String baseUrl, String endpoint, ReadableMap options, Promise promise) {
         createClientIfNeeded(baseUrl);
-        clientModule.get(baseUrl, endpoint, options, promise);
+        clientModule.get(baseUrl, endpoint, addHeaders(baseUrl, options), promise);
     }
 
     public static void post(String baseUrl, String endpoint, ReadableMap options, Promise promise) {
         createClientIfNeeded(baseUrl);
-        clientModule.post(baseUrl, endpoint, options, promise);
+        clientModule.post(baseUrl, endpoint, addHeaders(baseUrl, options), promise);
     }
 
     public static Response getSync(String baseUrl, String endpoint, ReadableMap options) {
         createClientIfNeeded(baseUrl);
-        return clientModule.getSync(baseUrl, endpoint, options);
+        return clientModule.getSync(baseUrl, endpoint, addHeaders(baseUrl, options));
     }
 
     public static Response postSync(String baseUrl, String endpoint, ReadableMap options) {
         createClientIfNeeded(baseUrl);
-        return clientModule.postSync(baseUrl, endpoint, options);
+        return clientModule.postSync(baseUrl, endpoint, addHeaders(baseUrl, options));
     }
 
     private static void createClientOptions() {
