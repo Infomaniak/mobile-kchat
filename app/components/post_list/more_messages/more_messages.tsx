a5525dee41b8fb8921048973f1fc9f8dd84e155c
Leopold Jacquot <leopold.jacquot@infomaniak.com>
2023-10-13 18:38:23 +0200

    fix: do not crash when scroll to new messages

--- app/components/post_list/more_messages/more_messages.tsx
+++ app/components/post_list/more_messages/more_messages.tsx
@@ -213,7 +213,7 @@ const MoreMessages = ({
     }, [serverUrl, channelId]);
 
     const onPress = useCallback(() => {
-        if (pressed.current) {
+        if (pressed.current || newMessageLineIndex <= 0) {
             return;
         }
 
