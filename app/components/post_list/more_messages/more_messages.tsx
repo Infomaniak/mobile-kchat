375138568714c3a72fa2829f936b76bd9cc24e71
Leopold Jacquot <leopold.jacquot@infomaniak.com>
2024-01-23 10:23:19 +0100

    fix: new message banner crash on click

--- app/components/post_list/more_messages/more_messages.tsx
+++ app/components/post_list/more_messages/more_messages.tsx
@@ -214,7 +214,7 @@ const MoreMessages = ({
     }, [serverUrl, channelId]);
 
     const onPress = useCallback(() => {
-        if (pressed.current) {
+        if (pressed.current || newMessageLineIndex <= 0) {
             return;
         }
 
