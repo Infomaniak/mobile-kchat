9108151f7bda25ce55bb4e2ea5e3325e18b0db6e
Leopold Jacquot <leopold.jacquot@infomaniak.com>
2024-02-06 10:13:15 +0100

    fix: allowed themes

--- app/queries/servers/system.ts
+++ app/queries/servers/system.ts
@@ -5,7 +5,7 @@ import {Database, Q} from '@nozbe/watermelondb';
 import {of as of$, Observable, combineLatest} from 'rxjs';
 import {switchMap, distinctUntilChanged} from 'rxjs/operators';
 
-import {Preferences, License} from '@constants';
+import {License} from '@constants';
 import {MM_TABLES, SYSTEM_IDENTIFIERS} from '@constants/database';
 import {PUSH_PROXY_STATUS_UNKNOWN} from '@constants/push_proxy';
 import {isMinimumServerVersion} from '@utils/helpers';
@@ -501,18 +501,9 @@ export const observeOnlyUnreads = (database: Database) => {
 };
 
 export const observeAllowedThemesKeys = (database: Database) => {
-    const defaultThemeKeys = Object.keys(Preferences.THEMES);
     return observeConfigValue(database, 'AllowedThemes').pipe(
-        switchMap((allowedThemes) => {
-            let acceptableThemes = defaultThemeKeys;
-            if (allowedThemes) {
-                const allowedThemeKeys = (allowedThemes ?? '').split(',').filter(String);
-                if (allowedThemeKeys.length) {
-                    acceptableThemes = defaultThemeKeys.filter((k) => allowedThemeKeys.includes(k));
-                }
-            }
-
-            return of$(acceptableThemes);
+        switchMap(() => {
+            return of$(['quartz', 'infomaniak', 'onyx']);
         }),
     );
 };
