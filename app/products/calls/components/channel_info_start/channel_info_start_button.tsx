dd5463f7bbfb5a3eb2930b60252c2b49cdc5b0a9
Tim DE WINTER <tim.dewinter@infomaniak.com>
2024-05-22 10:51:40 +0200

    feat: start call button now pops the call screen

--- app/products/calls/components/channel_info_start/channel_info_start_button.tsx
+++ app/products/calls/components/channel_info_start/channel_info_start_button.tsx
@@ -3,15 +3,20 @@
 
 import React, {useCallback} from 'react';
 import {useIntl} from 'react-intl';
+import {Platform} from 'react-native';
 
+import {Screens} from '@app/constants';
 import {leaveCall} from '@calls/actions';
 import {leaveAndJoinWithAlert, showLimitRestrictedAlert} from '@calls/alerts';
 import {useTryCallsFunction} from '@calls/hooks';
 import OptionBox from '@components/option_box';
+import {allOrientations, dismissAllModalsAndPopToScreen} from '@screens/navigation';
 import CallManager from '@store/CallManager';
 import {preventDoubleTap} from '@utils/tap';
 
+import type {CallPassedProps} from '../../screens/call_screen/call_screen';
 import type {LimitRestrictedInfo} from '@calls/observers';
+import type {Options} from 'react-native-navigation';
 
 export interface Props {
     serverUrl: string;
@@ -31,7 +36,39 @@ const ChannelInfoStartButton = ({
     limitRestrictedInfo,
 }: Props) => {
     const intl = useIntl();
+    const {formatMessage} = intl;
     const isLimitRestricted = limitRestrictedInfo.limitRestricted;
+
+    /**
+     * Upon pressing "Start call", dial and join the kMeet
+     * Pop the CALL screen
+     */
+    const onStartCall = useCallback(preventDoubleTap(async () => {
+        // Start a new call via API
+        const call = await CallManager.startCall(serverUrl, channelId);
+
+        if (call !== null) {
+            // Pop the CALL screen
+            const title = formatMessage({id: 'mobile.calls_call_screen', defaultMessage: 'Call'});
+            const passedProps: CallPassedProps = {
+                serverUrl: call.server_url,
+                channelId: call.channel_id,
+                conferenceId: call.id,
+            };
+            const options: Options = {
+                layout: {
+                    backgroundColor: '#000',
+                    componentBackgroundColor: '#000',
+                    orientation: allOrientations,
+                },
+                topBar: {
+                    background: {color: '#000'},
+                    visible: Platform.OS === 'android',
+                },
+            };
+            await dismissAllModalsAndPopToScreen(Screens.CALL, title, passedProps, options);
+        }
+    }), [channelId, formatMessage, serverUrl]);
 
     const toggleJoinLeave = useCallback(() => {
         if (alreadyInCall) {
@@ -47,15 +84,13 @@ const ChannelInfoStartButton = ({
 
     const [msgPostfix] = useTryCallsFunction(toggleJoinLeave);
 
-    const joinText = intl.formatMessage({id: 'mobile.calls_join_call', defaultMessage: 'Join call'});
-    const startText = intl.formatMessage({id: 'mobile.calls_start_call', defaultMessage: 'Start call'});
-    const leaveText = intl.formatMessage({id: 'mobile.calls_leave_call', defaultMessage: 'Leave call'});
+    const joinText = formatMessage({id: 'mobile.calls_join_call', defaultMessage: 'Join call'});
+    const startText = formatMessage({id: 'mobile.calls_start_call', defaultMessage: 'Start call'});
+    const leaveText = formatMessage({id: 'mobile.calls_leave_call', defaultMessage: 'Leave call'});
 
     return (
         <OptionBox
-            onPress={preventDoubleTap(() => {
-                CallManager.startCall(serverUrl, channelId);
-            })}
+            onPress={onStartCall}
             text={startText}
             iconName='phone'
             activeText={joinText + msgPostfix}
