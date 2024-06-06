315487413270722393a8236ec5cba0cc6d652eec
Tim DE WINTER <tim.dewinter@infomaniak.com>
2024-06-06 12:41:44 +0200

    fix: do not allow triggering leaveCall if CallScreen is unmounted

--- app/hooks/utils.ts
+++ app/hooks/utils.ts
@@ -1,11 +1,23 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {useRef, type RefObject} from 'react';
+import {useEffect, useRef, type RefObject} from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-constraint
 export const useTransientRef = <T extends unknown>(value: T): RefObject<T> => {
     const ref = useRef<T>(value);
     ref.current = value;
     return ref;
+};
+
+export const useMountedRef = () => {
+    const mountedRef = useRef(false);
+    useEffect(() => {
+        mountedRef.current = true;
+        return () => {
+            mountedRef.current = false;
+        };
+    }, []);
+
+    return mountedRef;
 };
