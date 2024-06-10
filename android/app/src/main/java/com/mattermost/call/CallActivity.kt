230db42d1ae38621c85a1fd6e992de48b70462f2
Leopold Jacquot <leopold.jacquot@infomaniak.com>
2024-06-10 14:15:22 +0200

    ci: fix android build

--- android/app/src/main/java/com/mattermost/call/CallActivity.kt
+++ android/app/src/main/java/com/mattermost/call/CallActivity.kt
@@ -13,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationManagerCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.mattermost.notification.NotificationUtils
+import com.mattermost.rnbeta.*
 import com.mattermost.rnbeta.databinding.ActivityCallBinding
 
 class CallActivity : AppCompatActivity() {
