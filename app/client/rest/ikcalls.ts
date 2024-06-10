9fdfd37a3a74f07b2a95520a29ebe88e20cb12cf
Tim DE WINTER <tim.dewinter@infomaniak.com>
2024-06-10 15:38:22 +0200

    feat: added client cancel call

--- app/client/rest/ikcalls.ts
+++ app/client/rest/ikcalls.ts
@@ -13,59 +13,37 @@ export type ApiCall = {
 
 export interface IKClientCallsMix {
     getIKCalls: () => Promise<[ApiCall]>;
-    leaveCall: (conferenceId: string) => Promise<ApiCall>;
-    declineCall: (conferenceId: string) => Promise<ApiCall>;
     answerCall: (conferenceId: string) => Promise<ApiCall>;
+    cancelCall: (conferenceId: string) => Promise<ApiCall>;
+    declineCall: (conferenceId: string) => Promise<ApiCall>;
+    leaveCall: (conferenceId: string) => Promise<ApiCall>;
     startCall: (channelId: string) => Promise<ApiCall>;
 }
 
 const IKClientCalls = (superclass: any) => class extends superclass {
-    getCallsRoute(): string {
-        return `${this.urlVersion}/conferences`;
-    }
+    // Routes
+    getCallsRoute = (suffix?: string): string =>
+        `${this.urlVersion}/conferences${suffix || ''}`;
+    getCallRoute = (suffix?: string) => (conferenceId: string) =>
+        this.getCallsRoute(`/${conferenceId}${suffix || ''}`);
 
-    getLeaveCallRoute(conferenceId: string): string {
-        return `${this.getCallsRoute()}/${conferenceId}/leave`;
-    }
+    getAnswerCallRoute = this.getCallRoute('/answer');
+    getCancelCallRoute = this.getCallRoute('/cancel');
+    getDeclineCallRoute = this.getCallRoute('/decline');
+    getLeaveCallRoute = this.getCallRoute('/leave');
 
-    getAnswerCallRoute(conferenceId: string): string {
-        return `${this.getCallsRoute()}/${conferenceId}/answer`;
-    }
-
-    getDeclineCallRoute(conferenceId: string): string {
-        return `${this.getCallsRoute()}/${conferenceId}/decline`;
-    }
-
-    getIKCalls = async () => {
-        return this.doFetch(
-            `${this.getCallsRoute()}`,
-            {method: 'get'},
-        );
-    };
-    leaveCall = async (conferenceId: string) => {
-        return this.doFetch(
-            `${this.getLeaveCallRoute(conferenceId)}`,
-            {method: 'post'},
-        );
-    };
-    declineCall = async (conferenceId: string) => {
-        return this.doFetch(
-            `${this.getDeclineCallRoute(conferenceId)}`,
-            {method: 'post'},
-        );
-    };
-    answerCall = async (conferenceId: string) => {
-        return this.doFetch(
-            `${this.getAnswerCallRoute(conferenceId)}`,
-            {method: 'post'},
-        );
-    };
-    startCall = async (channelId: string) => {
-        return this.doFetch(
-            `${this.getCallsRoute()}`,
-            {method: 'post', body: {channel_id: channelId}},
-        );
-    };
+    // Queries
+    getIKCalls = () => this.doFetch(`${this.getCallsRoute()}`, {method: 'get'});
+    answerCall = (conferenceId: string) =>
+        this.doFetch(`${this.getAnswerCallRoute(conferenceId)}`, {method: 'post'});
+    cancelCall = (conferenceId: string) =>
+        this.doFetch(`${this.getCancelCallRoute(conferenceId)}`, {method: 'post'});
+    declineCall = (conferenceId: string) =>
+        this.doFetch(`${this.getDeclineCallRoute(conferenceId)}`, {method: 'post'});
+    leaveCall = (conferenceId: string) =>
+        this.doFetch(`${this.getLeaveCallRoute(conferenceId)}`, {method: 'post'});
+    startCall = (channelId: string) =>
+        this.doFetch(`${this.getCallsRoute()}`, {method: 'post', body: {channel_id: channelId}});
 };
 
 export default IKClientCalls;
