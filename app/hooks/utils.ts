cf4912161b65a791b5e3f4674cdfca45d9e9ce9c
Tim DE WINTER <tim.dewinter@infomaniak.com>
2024-06-10 15:37:22 +0200

    chore: added useRerender hook

--- app/hooks/utils.ts
+++ app/hooks/utils.ts
@@ -1,7 +1,17 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {useEffect, useRef, type RefObject} from 'react';
+import {useCallback, useEffect, useRef, useState, type RefObject} from 'react';
+
+/**
+ * Return a callback that forces a re-render
+ */
+export const useRerender = () => {
+    const [, setState] = useState(0);
+    return useCallback(() => {
+        setState((n) => n + 1);
+    }, []);
+};
 
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-constraint
 export const useTransientRef = <T extends unknown>(value: T): RefObject<T> => {
