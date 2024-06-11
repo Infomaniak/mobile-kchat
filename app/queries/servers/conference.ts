cdc80c68f12bb8a3901e82e2e6684edb58b1f5b7
Tim DE WINTER <tim.dewinter@infomaniak.com>
2024-06-11 13:43:07 +0200

    fix: added observeConferenceParticipantApprovedCount

--- app/queries/servers/conference.ts
+++ app/queries/servers/conference.ts
@@ -12,13 +12,13 @@ import type ConferenceParticipantModel from '@typings/database/models/servers/co
 
 const {CONFERENCE, CONFERENCE_PARTICIPANT} = DatabaseConstants.MM_TABLES.SERVER;
 
-export const observeConference = (database: Database, conferenceId: string) => {
-    return database.
-        get<ConferenceModel>(CONFERENCE).
-        query(Q.where('id', conferenceId), Q.take(1)).
-        observe().
+export const queryConference = (database: Database, conferenceId: string) =>
+    database.get<ConferenceModel>(CONFERENCE).query(Q.where('id', conferenceId));
+
+export const observeConference = (database: Database, conferenceId: string) =>
+    queryConference(database, conferenceId).
+        observeWithColumns(['delete_at']).
         pipe(switchMap((cs) => of$(cs.length === 1 ? cs[0] : undefined)));
-};
 
 export const observeConferenceParticipants = (database: Database, conferenceId: string, limit?: number) => {
     const clauses: Q.Clause[] = [Q.where('conference_id', conferenceId), Q.sortBy('id', Q.asc)];
@@ -32,9 +32,28 @@ export const observeConferenceParticipants = (database: Database, conferenceId: 
         observeWithColumns(['status']);
 };
 
-export const observeConferenceParticipantCount = (database: Database, conferenceId: string) => {
-    return database.
+export const observeConferenceParticipantCount = (database: Database, conferenceId: string) =>
+    database.
         get<ConferenceParticipantModel>(CONFERENCE_PARTICIPANT).
         query(Q.where('conference_id', conferenceId)).
+        observeCount();
+
+/**
+ * Observe the number of conference participants that are in an "approved" status
+ * (ie. they are currently in the call)
+ * Possibly ignore a user ids (the caller)
+ */
+export const observeConferenceParticipantApprovedCount = (database: Database, conferenceId: string, ignoredUserId?: string) => {
+    const clauses: Q.Where[] = [
+        Q.where('conference_id', conferenceId),
+        Q.where('status', 'approved'),
+    ];
+    if (typeof ignoredUserId === 'string') {
+        clauses.push(Q.where('user_id', Q.notEq(ignoredUserId)));
+    }
+
+    return database.
+        get<ConferenceParticipantModel>(CONFERENCE_PARTICIPANT).
+        query(Q.and(...clauses)).
         observeCount();
 };
