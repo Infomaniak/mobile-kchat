2c21278d851fbd1947f26b52ca49fe07b111e4de
Vincent TE <vincent.te@infomaniak.com>
2024-06-12 08:23:16 +0200

    Fix how we ask the user to enter his password

--- android/app/src/main/java/com/mattermost/call/CallActivity.kt
+++ android/app/src/main/java/com/mattermost/call/CallActivity.kt
@@ -14,7 +14,6 @@ import androidx.core.app.NotificationManagerCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.mattermost.notification.NotificationUtils
 import com.mattermost.notification.NotificationUtils.dismissCallNotification
-import com.mattermost.rnbeta.*
 import com.mattermost.rnbeta.databinding.ActivityCallBinding
 
 class CallActivity : AppCompatActivity() {
@@ -22,20 +21,21 @@ class CallActivity : AppCompatActivity() {
     private val binding by lazy { ActivityCallBinding.inflate(layoutInflater) }
 
     private val callManagerModule by lazy { CallManagerModule.getInstance() }
+    private val conferenceId by lazy {
+        intent.getStringExtra(NotificationUtils.INTENT_EXTRA_CONFERENCE_ID_KEY)
+    }
+    private val channelName by lazy {
+        intent.getStringExtra(NotificationUtils.INTENT_EXTRA_CHANNEL_NAME_KEY)
+    }
     private val channelId by lazy {
         intent.getStringExtra(NotificationUtils.INTENT_EXTRA_CHANNEL_ID_KEY)
     }
     private val serverId by lazy {
         intent.getStringExtra(NotificationUtils.INTENT_EXTRA_SERVER_ID_KEY)
     }
-    private val channelName by lazy {
-        intent.getStringExtra(NotificationUtils.INTENT_EXTRA_CHANNEL_NAME_KEY)
-    }
     private val conferenceJWT by lazy {
         intent.getStringExtra(NotificationUtils.INTENT_EXTRA_CONFERENCE_JWT_KEY)
     }
-
-    private var conferenceId: String? = null
 
     private val localBroadcastManager by lazy { LocalBroadcastManager.getInstance(this) }
 
@@ -54,20 +54,13 @@ class CallActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) = with(binding) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        configureActivityOverLockScreen()
 
-        // Getting this here because if we're using a lazy, the value will not be the right one
-        conferenceId = intent.getStringExtra(NotificationUtils.INTENT_EXTRA_CONFERENCE_ID_KEY)
+        configureActivityOverLockScreen()
 
         idCaller.text = channelName
         answerButton.setOnClickListener {
             conferenceId?.let { dismissCallNotification(it) }
-            callManagerModule?.callAnswered(
-                serverId = serverId!!,
-                channelId = channelId!!,
-                conferenceJWT = conferenceJWT!!
-            )
-            leaveActivity()
+            askToUnlockPhone()
         }
         declineButton.setOnClickListener {
             conferenceId?.let { dismissCallNotification(it) }
@@ -91,22 +84,29 @@ class CallActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
             setShowWhenLocked(true)
             setTurnScreenOn(true)
-            val keyguardManager = getSystemService(KEYGUARD_SERVICE) as KeyguardManager
-            keyguardManager.requestDismissKeyguard(this, object : KeyguardDismissCallback() {
-                override fun onDismissSucceeded() {
-                    super.onDismissSucceeded()
-                    callManagerModule?.callEnded(
-                        serverId = serverId!!,
-                        conferenceId = conferenceId!!
-                    )
-                }
-            })
         } else {
             this.window.addFlags(
                 WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
                         WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
                         WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
             )
+        }
+    }
+
+    private fun askToUnlockPhone() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            val keyguardManager = getSystemService(KEYGUARD_SERVICE) as KeyguardManager
+            keyguardManager.requestDismissKeyguard(this, object : KeyguardDismissCallback() {
+                override fun onDismissSucceeded() {
+                    super.onDismissSucceeded()
+                    callManagerModule?.callAnswered(
+                        serverId = serverId!!,
+                        channelId = channelId!!,
+                        conferenceJWT = conferenceJWT!!
+                    )
+                    leaveActivity()
+                }
+            })
         }
     }
 
