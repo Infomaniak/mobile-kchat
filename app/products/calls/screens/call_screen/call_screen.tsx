565f6f6a6f0ab0b4aec4d808e5f7cc559497106f
Tim DE WINTER <tim.dewinter@infomaniak.com>
2024-06-18 13:26:00 +0200

    chore: removed auto-update status code

--- app/products/calls/screens/call_screen/call_screen.tsx
+++ app/products/calls/screens/call_screen/call_screen.tsx
@@ -4,31 +4,25 @@
 /* eslint-disable max-nested-callbacks */
 
 import {JitsiMeeting, type JitsiRefProps} from '@jitsi/react-native-sdk';
-import moment from 'moment';
 import React, {useCallback, useEffect, useImperativeHandle, useMemo, useRef, useState, type ComponentProps, type MutableRefObject} from 'react';
 import {useIntl} from 'react-intl';
-import {ActivityIndicator, DeviceEventEmitter, FlatList, NativeModules, View} from 'react-native';
+import {ActivityIndicator, FlatList, NativeModules, View} from 'react-native';
 import {Navigation} from 'react-native-navigation';
 import {useAnimatedStyle} from 'react-native-reanimated';
 import {SafeAreaView, type Edge} from 'react-native-safe-area-context';
 
-import {updateLocalCustomStatus} from '@actions/local/user';
 import {fetchChannelMemberships, switchToChannelById} from '@actions/remote/channel';
 import {fetchConference} from '@actions/remote/conference';
-import {unsetCustomStatus, updateCustomStatus} from '@actions/remote/user';
 import {postListRef} from '@app/components/post_list/post_list';
-import {CustomStatusDurationEnum, SET_CUSTOM_STATUS_FAILURE} from '@app/constants/custom_status';
 import {useServerId} from '@app/context/server';
 import {useTheme} from '@app/context/theme';
 import {useCollapsibleHeader} from '@app/hooks/header';
 import {useMountedRef, useRerender, useTransientRef} from '@app/hooks/utils';
 import {fetchConferenceHasAtLeastOneParticipantPresent} from '@app/queries/servers/conference';
 import {getCommonSystemValues} from '@app/queries/servers/system';
-import {calculateExpiryTime} from '@app/screens/custom_status/custom_status';
 import {logError} from '@app/utils/log';
 import {changeOpacity, makeStyleSheetFromTheme} from '@app/utils/theme';
 import {typography} from '@app/utils/typography';
-import {getUserCustomStatus, getUserTimezone} from '@app/utils/user';
 import {usePermissionsChecker} from '@calls/hooks';
 import {AudioMuteButton, ContentContainer, HangupButton, OutgoingRinging, ToolboxContainer, VideoMuteButton} from '@calls/screens/call_screen/jitsi_components';
 import RippleIcon from '@calls/screens/call_screen/ripple_icon';
@@ -36,13 +30,12 @@ import NavigationHeader from '@components/navigation_header';
 import Image from '@components/profile_picture/image';
 import {General, Screens} from '@constants';
 import DatabaseManager from '@database/manager';
-import {debounce, noop} from '@helpers/api/general';
+import {noop} from '@helpers/api/general';
 import CallManager from '@store/CallManager';
 import {isDMorGM as isChannelDMorGM} from '@utils/channel';
 
 import type ChannelModel from '@typings/database/models/servers/channel';
 import type ConferenceModel from '@typings/database/models/servers/conference';
-import type UserModel from '@typings/database/models/servers/user';
 
 export type PassedProps = {
     serverUrl: string;
@@ -58,7 +51,6 @@ export type PassedProps = {
 export type InjectedProps = {
     channel?: ChannelModel;
     conference: ConferenceModel | undefined;
-    currentUser: UserModel;
     currentUserId: string;
     micPermissionsGranted: boolean;
     participantCount: number;
@@ -74,8 +66,7 @@ export type CallScreenHandle = {
 
 export const callScreenRef = React.createRef<CallScreenHandle>();
 
-type Props = PassedProps & InjectedProps & { autoUpdateStatus: boolean }
-type UserStatus = ReturnType<typeof getUserCustomStatus>
+type Props = PassedProps & InjectedProps
 
 /**
  * Prevent the conference from being closed by 'leaveCall'
@@ -91,11 +82,6 @@ const MINIMUM_CONFERENCE_DURATION = 2000; // ms
 const MAX_CALLNAME_PARTICIPANT_USERNAMES = 2;
 
 const EDGES: Edge[] = ['bottom', 'left', 'right'];
-
-const kMeetStatus = {
-    emoji: 'kmeet',
-    duration: CustomStatusDurationEnum.DONT_CLEAR,
-} as Pick<UserCustomStatus, 'emoji' | 'duration'>;
 
 /**
  * Global styles
@@ -145,72 +131,6 @@ const getStyleSheet = makeStyleSheetFromTheme((theme) => ({
         color: theme.buttonColor,
     },
 }));
-
-/**
- * Convert possible "duration" status to a specific "date_and_time"
- * used for comparing equality of custom statuses
- * If the status has expired, returns undefined
- */
-const asAbsoluteStatus = (status: UserStatus, currentUser: UserModel) => {
-    if (typeof status === 'undefined') {
-        return undefined;
-    }
-
-    const absoluteStatus = ({
-        ...status,
-        ...(status.duration === CustomStatusDurationEnum.DATE_AND_TIME ? {} : {
-            expires_at: calculateExpiryTime(status.duration!, currentUser),
-            duration: CustomStatusDurationEnum.DATE_AND_TIME,
-        }),
-    });
-
-    const hasExpired = (() => {
-        if (absoluteStatus.duration === CustomStatusDurationEnum.DONT_CLEAR || !absoluteStatus.duration) {
-            return false;
-        }
-
-        const expiryTime = moment(absoluteStatus.expires_at);
-        const timezone = getUserTimezone(currentUser);
-        const currentTime = timezone ? moment.tz(timezone) : moment();
-        return currentTime.isSameOrAfter(expiryTime);
-    })();
-
-    return hasExpired ? undefined : absoluteStatus;
-};
-
-/**
- * Compute custom status equality
- */
-const isStatusEqual = (aStatus: UserStatus, bStatus: UserStatus, currentUser: UserModel): boolean => {
-    const a = asAbsoluteStatus(aStatus, currentUser);
-    const b = asAbsoluteStatus(bStatus, currentUser);
-
-    // One is undefined, the other isn't
-    if (typeof a !== typeof b) {
-        return false;
-    }
-
-    // Both are undefined
-    if (
-        typeof a === 'undefined' ||
-        typeof b === 'undefined'
-    ) {
-        return true;
-    }
-
-    // Same emoji, same text, same expiry
-    return (a.emoji === b.emoji && a.text === b.text && a.expires_at === b.expires_at);
-};
-
-/**
- * Test if a status is "In a meeting"
- */
-const isStatusKMeet = (status: UserStatus): boolean =>
-    typeof status !== 'undefined' &&
-    status.emoji === kMeetStatus.emoji && (
-        status.duration === CustomStatusDurationEnum.DONT_CLEAR ||
-        status.duration === null
-    );
 
 /**
  * Test if setting a new value to a ref would change it
@@ -314,13 +234,11 @@ const nativeReporters = {
 };
 
 const CallScreen = ({
-    autoUpdateStatus = false,
     channel,
     channelId,
     conference,
     conferenceId,
     conferenceJWT,
-    currentUser,
     currentUserId,
     micPermissionsGranted,
     participantCount,
@@ -522,62 +440,6 @@ const CallScreen = ({
     );
 
     /**
-     * Compare current status and new (arg) status
-     * Only update the status if it's not the same
-     * Refs are needed to prevent re-creating a new debounced function
-     */
-    const currentUserRef = useTransientRef<UserModel>(currentUser);
-    const doUpdateStatus = useCallback(async (status: UserCustomStatus | undefined) => {
-        if (
-            /**
-             * Compare current status and new (arg) status
-             * Only update the status if it's not the same
-             */
-            !isStatusEqual(status, getUserCustomStatus(currentUserRef.current!), currentUserRef.current!)
-        ) {
-            const {error} = await (typeof status === 'undefined' ? unsetCustomStatus(serverUrl) : updateCustomStatus(serverUrl, status));
-            if (error) {
-                DeviceEventEmitter.emit(SET_CUSTOM_STATUS_FAILURE);
-                return;
-            }
-
-            updateLocalCustomStatus(serverUrl, currentUserRef.current!, status);
-        }
-    }, [serverUrl]);
-    const doUpdateStatusRef = useTransientRef(doUpdateStatus);
-
-    /**
-     * Handle setting-up the "In a call" custom status
-     * preserve previous status and restore it after ending the call
-     */
-    const statusBeforeCallRef = useRef<UserStatus>(undefined);
-    const updateStatus = useCallback(async () => {
-        // Save previous status
-        // /!\ This one needs to be absolute since ending the status in 1 hour after the
-        // call ended might not be the same time
-        statusBeforeCallRef.current = asAbsoluteStatus(getUserCustomStatus(currentUserRef.current!), currentUserRef.current!);
-
-        // Overwrite status with kMeet
-        doUpdateStatus({
-            ...kMeetStatus,
-            text: formatMessage({id: 'custom_status.suggestions.in_a_meeting', defaultMessage: 'In a meeting'}),
-        });
-    }, [formatMessage]);
-    const restoreStatus = useMemo(
-        () => debounce(() => {
-            // Only restore the previous status if the user did not cleared the "kMeet" status
-            // this prevents statuses being restored out of nowhere
-            if (
-                typeof doUpdateStatusRef.current === 'function' &&
-                isStatusKMeet(getUserCustomStatus(currentUserRef.current!))
-            ) {
-                doUpdateStatusRef.current(statusBeforeCallRef.current);
-            }
-        }, 2000, {leading: true, trailing: false}),
-        [],
-    );
-
-    /**
      * Close the current JitsiMeeting
      * Also trigger the "leaveCall" API
      */
@@ -602,11 +464,6 @@ const CallScreen = ({
             ) &&
             hasUpdatedRef(leavingRef, true)
         ) {
-            // Restore previous status
-            if (autoUpdateStatus) {
-                restoreStatus();
-            }
-
             // Remove the call screen, and in some situations it needs to be removed twice before actually being removed
             Navigation.pop(Screens.CALL).catch(() => null);
             Navigation.pop(Screens.CALL).catch(() => null);
@@ -668,10 +525,6 @@ const CallScreen = ({
             // Update the jitsiMeetingMountedAtRef date
             jitsiMeetingMountedRef.current = true;
             jitsiMeetingMountedAtRef.current = Date.now();
-
-            if (autoUpdateStatus) {
-                updateStatus();
-            }
 
             // Report that the call started only if the initiator is not native
             //  -> 'internal' initiator means react-native
--- app/products/calls/screens/call_screen/index.tsx
+++ app/products/calls/screens/call_screen/index.tsx
@@ -6,40 +6,32 @@ import {of as of$} from 'rxjs';
 import {distinctUntilChanged, switchMap} from 'rxjs/operators';
 
 import {observeChannel} from '@app/queries/servers/channel';
-import {observeConference, observeConferenceParticipantCount/*, observeConferenceHasAtLeastOneParticipantPresent*/} from '@app/queries/servers/conference';
+import {observeConference, observeConferenceParticipantCount} from '@app/queries/servers/conference';
 import CallScreen, {type PassedProps} from '@calls/screens/call_screen/call_screen';
 import {observeGlobalCallsState} from '@calls/state';
 import {observeCurrentUserId} from '@queries/servers/system';
-import {observeUser} from '@queries/servers/user';
 
 import type {WithDatabaseArgs} from '@typings/database/database';
 
 const enhance = withObservables(['channelId', 'conferenceId'], (
     {channelId, conferenceId, database: db}:
     WithDatabaseArgs & Pick<PassedProps, 'channelId' | 'conferenceId'>,
-) => {
-    const currentUserId = observeCurrentUserId(db);
+) => ({
+    channel: observeChannel(db, channelId),
+    currentUserId: observeCurrentUserId(db),
 
-    return {
-        channel: observeChannel(db, channelId),
-        currentUserId: observeCurrentUserId(db),
-        currentUser: currentUserId.pipe(
-            switchMap((userId) => observeUser(db, userId)),
-        ),
+    micPermissionsGranted: observeGlobalCallsState().pipe(
+        switchMap((gs) => of$(gs.micPermissionsGranted)),
+        distinctUntilChanged(),
+    ),
 
-        micPermissionsGranted: observeGlobalCallsState().pipe(
-            switchMap((gs) => of$(gs.micPermissionsGranted)),
-            distinctUntilChanged(),
-        ),
+    conference: observeConference(db, conferenceId),
+    participantCount: observeConferenceParticipantCount(db, conferenceId),
 
-        conference: observeConference(db, conferenceId),
-        participantCount: observeConferenceParticipantCount(db, conferenceId),
-
-        // hasAtLeastOneParticipantPresent: currentUserId.pipe(
-        //     switchMap((userId) => observeConferenceHasAtLeastOneParticipantPresent(db, conferenceId, userId)),
-        //     distinctUntilChanged(),
-        // ),
-    };
-});
+    // hasAtLeastOneParticipantPresent: currentUserId.pipe(
+    //     switchMap((userId) => observeConferenceHasAtLeastOneParticipantPresent(db, conferenceId, userId)),
+    //     distinctUntilChanged(),
+    // ),
+}));
 
 export default withDatabase(enhance(CallScreen));
