db9471c38f80b51fbe0ca8437c558ecf560c61d0
Tim DE WINTER <tim.dewinter@infomaniak.com>
2024-06-27 14:00:33 +0200

    fix(debug): Flipper initialization
    Ref. https://github.com/react-native-community/upgrade-support/issues/213

--- ios/Mattermost/AppDelegate.mm
+++ ios/Mattermost/AppDelegate.mm
@@ -12,6 +12,18 @@
 #import "kChat-Swift.h"
 #import <os/log.h>
 
+#if DEBUG
+#ifdef FB_SONARKIT_ENABLED
+#import <FlipperKit/FlipperClient.h>
+#import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
+#import <FlipperKitLayoutPlugin/SKDescriptorMapper.h>
+#import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
+#import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+#import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
+#import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
+#endif
+#endif
+
 @implementation AppDelegate
 
 NSString* const NOTIFICATION_MESSAGE_ACTION = @"message";
@@ -59,7 +71,23 @@ NSString* const NOTIFICATION_TEST_ACTION = @"test";
 
   os_log(OS_LOG_DEFAULT, "Mattermost started!!");
 
+  [self initializeFlipper:application];
+  
   return YES;
+}
+
+- (void) initializeFlipper:(UIApplication *)application {
+  #if DEBUG
+  #ifdef FB_SONARKIT_ENABLED
+    FlipperClient *client = [FlipperClient sharedClient];
+    SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
+    [client addPlugin: [[FlipperKitLayoutPlugin alloc] initWithRootNode: application withDescriptorMapper: layoutDescriptorMapper]];
+    [client addPlugin: [[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
+    [client addPlugin: [FlipperKitReactPlugin new]];
+    [client addPlugin: [[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
+    [client start];
+  #endif
+  #endif
 }
 
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
