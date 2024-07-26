7888c837d6bafb71b189c388fa42d91d2826ce36
Boris Trombert <boris.trombert@infomaniak.com>
2024-07-26 15:07:16 +0200

    Fix: errors syntax and channel name fix

--- app/components/post_list/post/preview_message/index.tsx
+++ app/components/post_list/post/preview_message/index.tsx
@@ -4,6 +4,7 @@ import {withDatabase, withObservables} from '@nozbe/watermelondb/react';
 import {switchMap} from '@nozbe/watermelondb/utils/rx';
 import {of as of$} from 'rxjs';
 
+import {General} from '@app/constants';
 import {observeChannel} from '@app/queries/servers/channel';
 import {observeConfigValue} from '@app/queries/servers/system';
 import {observeUser} from '@queries/servers/user';
@@ -17,11 +18,13 @@ const enhance = withObservables(['previewUserId', 'metadata'], ({database, previ
     const siteURL = observeConfigValue(database, 'SiteURL');
     let channel;
     let channelDisplayName;
-    if (metadata.channel_id) {
+    if (metadata.channel_id && metadata.channel_type === General.DM_CHANNEL) {
         channel = observeChannel(database, metadata.channel_id);
         channelDisplayName = channel.pipe(
             switchMap((c) => of$(c?.displayName)),
         );
+    } else {
+        channelDisplayName = of$(metadata.channel_display_name);
     }
 
     return {
