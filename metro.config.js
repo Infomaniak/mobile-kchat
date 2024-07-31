e639cdb97803366a7a0e5bec4990f4bee90d27ab
Tim DE WINTER <tim.dewinter@infomaniak.com>
2024-07-31 15:29:56 +0200

    fix: missing react svg file imports causing jitsi to crash when rendering icons

--- metro.config.js
+++ metro.config.js
@@ -4,6 +4,7 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
+const {assetExts, sourceExts} = defaultConfig.resolver;
 
 /**
  * Metro configuration
@@ -11,6 +12,15 @@ const defaultConfig = getDefaultConfig(__dirname);
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+    transformer: {
+        babelTransformerPath: require.resolve('react-native-svg-transformer/react-native'),
+    },
+    resolver: {
+        assetExts: assetExts.filter((ext) => ext !== 'svg'),
+        sourceExts: [...sourceExts, 'svg'],
+    },
+
+};
 
 module.exports = mergeConfig(defaultConfig, config);
