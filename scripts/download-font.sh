916d8b247c34e921b2520affc4c5de3f51b78859
Boris Trombert <boris.trombert@infomaniak.com>
2024-08-13 10:20:43 +0200

    fix: add semibold font

--- scripts/download-font.sh
+++ scripts/download-font.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 npm i --prefix ./tmp/font  @infomaniak/font-suisse@https://verdaccio.dev.infomaniak.ch/@infomaniak/font-suisse/-/font-suisse-1.0.0.tgz
-cp tmp/font/node_modules/@infomaniak/font-suisse/*.ttf ./assets/fonts/
+cp tmp/font/node_modules/@infomaniak/font-suisse/SuisseIntl-Medium.ttf ./assets/fonts/SuisseIntl-SemiBold.ttf
+cp tmp/font/node_modules/@infomaniak/font-suisse/SuisseIntl-Regular.ttf ./assets/fonts/
+# RegularItalic file is missing
 rm -rf tmp
