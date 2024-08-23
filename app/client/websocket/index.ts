0dd572a4e39876abe19a26b0665597a072d0c3db
Tim DE WINTER <tim.dewinter@infomaniak.com>
2024-08-23 08:12:23 +0200

    fix: pusher.js conn state not being all mapped to a mattermost websocket state

--- app/client/websocket/index.ts
+++ app/client/websocket/index.ts
@@ -76,7 +76,7 @@ export default class WebSocketClient {
             this.stop = false;
         }
 
-        if (this.conn && this.conn.connection.state !== WebSocketReadyState.CLOSED) {
+        if (this.conn && this.connState !== WebSocketReadyState.CLOSED) {
             return;
         }
 
@@ -110,7 +110,7 @@ export default class WebSocketClient {
             if (this.conn === client) {
                 // In case turning on/off Wi-fi on Samsung devices
                 // the websocket will call onClose then onError then initialize again with readyState CLOSED, we need to open it again
-                if (this.conn.connection.state === WebSocketReadyState.CLOSED) {
+                if (this.connState === WebSocketReadyState.CLOSED) {
                     clearTimeout(this.connectionTimeout);
                     this.connOpen();
                 }
@@ -206,7 +206,7 @@ export default class WebSocketClient {
 
     private async connOpen() {
         if (typeof this.conn !== 'undefined') {
-            if (this.conn.connection.state !== WebSocketReadyState.OPEN) {
+            if (this.connState !== WebSocketReadyState.OPEN) {
                 this.conn.connect();
             }
 
@@ -219,6 +219,27 @@ export default class WebSocketClient {
             this.bindChannel(`private-team.${user.team_id}`, false);
             this.bindChannel(`presence-user.${user.user_id}`, false);
             this.bindChannel(`presence-teamUser.${user.id}`);
+        }
+    }
+
+    private get connState() {
+        // Convert Pusher.js connection state to WebSocketReadyState
+        // Ref. https://pusher.com/docs/channels/using_channels/connection/#available-states
+        switch (this.conn?.connection.state) {
+            case 'initialized':
+            case 'connecting':
+                return WebSocketReadyState.CONNECTING;
+            case 'connected':
+                return WebSocketReadyState.OPEN;
+            case 'unavailable':
+            case 'failed':
+            case 'disconnected':
+                return WebSocketReadyState.CLOSED;
+            default:
+                return undefined;
+
+            // No Pusher.js equivalent
+            // return WebSocketReadyState.CLOSING;
         }
     }
 
@@ -269,7 +290,7 @@ export default class WebSocketClient {
             // with mattermost's .onOpen()
             if (
                 (eventName === 'connected') &&
-                (this.conn.connection.state === WebSocketReadyState.OPEN)
+                (this.connState === WebSocketReadyState.OPEN)
             ) {
                 fn();
             }
@@ -370,7 +391,7 @@ export default class WebSocketClient {
             data,
         };
 
-        if (this.conn && this.conn.connection.state === WebSocketReadyState.OPEN) {
+        if (this.conn && this.connState === WebSocketReadyState.OPEN) {
             if (typeof channel === 'undefined') {
                 // Global message
                 this.conn.send_event(action, msg);
@@ -378,7 +399,7 @@ export default class WebSocketClient {
                 // Channel message
                 channel.trigger(action, msg);
             }
-        } else if (!this.conn || this.conn.connection.state === WebSocketReadyState.CLOSED) {
+        } else if (!this.conn || this.connState === WebSocketReadyState.CLOSED) {
             this.conn = undefined;
             this.initialize(this.token);
         }
@@ -419,6 +440,6 @@ export default class WebSocketClient {
     }
 
     public isConnected(): boolean {
-        return this.conn?.connection.state === WebSocketReadyState.OPEN;
+        return this.connState === WebSocketReadyState.OPEN;
     }
 }
