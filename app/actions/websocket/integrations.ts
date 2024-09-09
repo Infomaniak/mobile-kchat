c3aaec6dfae2ae78ffb1b0080e3466c0e60fecaf
Leopold Jacquot <leopold.jacquot@infomaniak.com>
2024-09-09 22:45:43 +0200

    fix: open dialog

--- app/actions/websocket/integrations.ts
+++ app/actions/websocket/integrations.ts
@@ -5,18 +5,12 @@ import IntegrationsManager from '@managers/integrations_manager';
 import {getActiveServerUrl} from '@queries/app/servers';
 
 export async function handleOpenDialogEvent(serverUrl: string, msg: WebSocketMessage) {
-    const data: string = msg.data;
-    if (!data) {
+    const dialog: InteractiveDialogConfig = msg.data?.dialog;
+    if (!dialog) {
         return;
     }
 
     try {
-        const dialog: InteractiveDialogConfig = JSON.parse(data)?.dialog;
-
-        if (!dialog) {
-            return;
-        }
-
         const currentServer = await getActiveServerUrl();
         if (currentServer === serverUrl) {
             IntegrationsManager.getManager(serverUrl).setDialog(dialog);
@@ -24,4 +18,5 @@ export async function handleOpenDialogEvent(serverUrl: string, msg: WebSocketMes
     } catch {
         // Do nothing
     }
+    
 }
