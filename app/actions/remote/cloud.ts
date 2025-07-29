d43ad1b0b1a86d58aa7d14e7b4df501ece36ce08
Boris Trombert <boris.trombert@infomaniak.com>
2025-07-29 11:03:02 +0200

    fix: use static writer in model

--- app/actions/remote/cloud.ts
+++ app/actions/remote/cloud.ts
@@ -3,14 +3,13 @@
 import {Q} from '@nozbe/watermelondb';
 
 import {MM_TABLES} from '@app/constants/database';
+import {CloudUsageModel, LimitModel, type LimitModel as LimitModelType} from '@app/database/models/server';
 import {getFullErrorMessage} from '@app/utils/errors';
 import {logDebug} from '@app/utils/log';
 import DatabaseManager from '@database/manager';
 import NetworkManager from '@managers/network_manager';
 
 import {forceLogoutIfNecessary} from './session';
-
-import type {CloudUsageModel, LimitModel} from '@app/database/models/server';
 
 export const fetchCloudLimits = async (serverUrl: string) => {
     try {
@@ -22,32 +21,15 @@ export const fetchCloudLimits = async (serverUrl: string) => {
             return;
         }
 
-        const limitsCollection = database.collections.get<LimitModel>(MM_TABLES.SERVER.LIMIT);
-        const [limits] = await limitsCollection.query(Q.take(1)).fetch();
+        const limitsCollection = database.collections.get<LimitModelType>(MM_TABLES.SERVER.LIMIT);
+        const [limits] = await limitsCollection.query(
+            Q.where('id', 'limits'),
+        ).fetch();
 
         if (limits) {
             await limits.updateLimits(limitsFetch);
         } else {
-            await database.write(async () => {
-                await database.get<LimitModel>(MM_TABLES.SERVER.LIMIT).create((record) => {
-                    record.boards = limitsFetch.boards ?? {max: 0, warning: 0};
-                    record.bots = limitsFetch.bots ?? 0;
-                    record.custom_emojis = limitsFetch.custom_emojis ?? 0;
-                    record.guests = limitsFetch.guests ?? 0;
-                    record.incoming_webhooks = limitsFetch.incoming_webhooks ?? 0;
-                    record.integrations = limitsFetch.integrations ?? {max: 0, warning: 0};
-                    record.members = limitsFetch.members ?? 0;
-                    record.messages = limitsFetch.messages ?? {max: 0, warning: 0};
-                    record.outgoing_webhooks = limitsFetch.outgoing_webhooks ?? 0;
-                    record.private_channels = limitsFetch.private_channels ?? 0;
-                    record.public_channels = limitsFetch.public_channels ?? 0;
-                    record.reminder_custom_date = limitsFetch.reminder_custom_date ?? false;
-                    record.scheduled_draft_custom_date = limitsFetch.scheduled_draft_custom_date ?? false;
-                    record.sidebar_categories = limitsFetch.sidebar_categories ?? 0;
-                    record.storage = limitsFetch.storage ?? 0;
-                    record._raw.id = 'limits';
-                });
-            });
+            await LimitModel.createLimits(limitsCollection, limitsFetch);
         }
     } catch (error) {
         logDebug('error on fetchCloudLimits', getFullErrorMessage(error));
@@ -65,26 +47,14 @@ export const fetchUsage = async (serverUrl: string) => {
         }
 
         const usageCollection = database.collections.get<CloudUsageModel>(MM_TABLES.SERVER.USAGE);
-        const [usage] = await usageCollection.query(Q.take(1)).fetch();
+        const [usage] = await usageCollection.query(
+            Q.where('id', 'usage'),
+        ).fetch();
 
         if (usage) {
             await usage.updateUsage(usageFetch);
         } else {
-            await database.write(async () => {
-                await database.get<CloudUsageModel>(MM_TABLES.SERVER.USAGE).create((record) => {
-                    record._raw.id = 'usage';
-                    record.custom_emojis = usageFetch.custom_emojis ?? 0;
-                    record.guests = usageFetch.guests ?? 0;
-                    record.incoming_webhooks = usageFetch.incoming_webhooks ?? 0;
-                    record.members = usageFetch.members ?? 0;
-                    record.outgoing_webhooks = usageFetch.outgoing_webhooks ?? 0;
-                    record.pending_guests = usageFetch.pending_guests ?? 0;
-                    record.private_channels = usageFetch.private_channels ?? 0;
-                    record.public_channels = usageFetch.public_channels ?? 0;
-                    record.sidebar_categories = usageFetch.sidebar_categories ?? 0;
-                    record.storage = usageFetch.storage ?? 0;
-                });
-            });
+            await CloudUsageModel.createUsage(usageCollection, usageFetch);
         }
     } catch (error) {
         logDebug('error on fetchUsage', getFullErrorMessage(error));
--- app/database/models/server/usage.ts
+++ app/database/models/server/usage.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
+import {Collection, Model} from '@nozbe/watermelondb';
 import {field, writer} from '@nozbe/watermelondb/decorators';
-import Model from '@nozbe/watermelondb/Model';
 
 import {MM_TABLES} from '@app/constants/database';
 const {
@@ -10,6 +10,17 @@ const {
 
 export default class CloudUsageModel extends Model {
     static table = USAGE;
+
+    @field('custom_emojis') custom_emojis!: number;
+    @field('guests') guests!: number;
+    @field('incoming_webhooks') incoming_webhooks!: number;
+    @field('members') members!: number;
+    @field('outgoing_webhooks') outgoing_webhooks!: number;
+    @field('pending_guests') pending_guests!: number;
+    @field('private_channels') private_channels!: number;
+    @field('public_channels') public_channels!: number;
+    @field('sidebar_categories') sidebar_categories!: number;
+    @field('storage') storage!: number;
 
     @writer async updateUsage(usage: Partial<CloudUsageModel>) {
         await this.update((record) => {
@@ -26,14 +37,19 @@ export default class CloudUsageModel extends Model {
         });
     }
 
-    @field('custom_emojis') custom_emojis!: number;
-    @field('guests') guests!: number;
-    @field('incoming_webhooks') incoming_webhooks!: number;
-    @field('members') members!: number;
-    @field('outgoing_webhooks') outgoing_webhooks!: number;
-    @field('pending_guests') pending_guests!: number;
-    @field('private_channels') private_channels!: number;
-    @field('public_channels') public_channels!: number;
-    @field('sidebar_categories') sidebar_categories!: number;
-    @field('storage') storage!: number;
+    @writer static async createUsage(collection: Collection<CloudUsageModel>, usage: Partial<CloudUsageModel>) {
+        await collection.create((record) => {
+            record._raw.id = 'usage';
+            record.custom_emojis = usage.custom_emojis ?? 0;
+            record.guests = usage.guests ?? 0;
+            record.incoming_webhooks = usage.incoming_webhooks ?? 0;
+            record.members = usage.members ?? 0;
+            record.outgoing_webhooks = usage.outgoing_webhooks ?? 0;
+            record.pending_guests = usage.pending_guests ?? 0;
+            record.private_channels = usage.private_channels ?? 0;
+            record.public_channels = usage.public_channels ?? 0;
+            record.sidebar_categories = usage.sidebar_categories ?? 0;
+            record.storage = usage.storage ?? 0;
+        });
+    }
 }
