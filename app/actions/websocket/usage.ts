6dc210919f39e6a9b4e5f771122cc7433970139d
Boris Trombert <boris.trombert@infomaniak.com>
2025-08-22 17:15:05 +0200

    fix(ws): limitation change event

--- app/actions/websocket/usage.ts
+++ app/actions/websocket/usage.ts
@@ -2,7 +2,21 @@
 // See LICENSE.txt for license information.
 
 import {fetchUsage} from '@actions/remote/cloud';
+import DatabaseManager from '@database/manager';
+import {getCurrentTeamId} from '@queries/servers/system';
 
 export async function handleLimitationChanged(serverUrl: string) {
-    await fetchUsage(serverUrl);
+    const activeServerUrl = await DatabaseManager.getActiveServerUrl();
+    if (!activeServerUrl) {
+        throw new Error('No active server URL found');
+    }
+    const operator = DatabaseManager.serverDatabases[activeServerUrl]?.operator;
+    if (!operator) {
+        throw new Error('Cannot find server database');
+    }
+    const {database} = operator;
+
+    const currentTeamId = await getCurrentTeamId(database);
+
+    await fetchUsage(serverUrl, currentTeamId);
 }
