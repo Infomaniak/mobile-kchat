29b4447cb1e6b024ce41490e12e8ee417bb97eab
Aurelien SBERRO <aurelien.sberro@infomaniak.com>
2025-09-05 15:07:04 +0200

    fix(global_threads): adjust context style to include shared top inset value

--- app/screens/global_threads/global_threads.tsx
+++ app/screens/global_threads/global_threads.tsx
@@ -19,6 +19,7 @@ import {useTeamSwitch} from '@hooks/team_switch';
 import useTabs, {type TabDefinition} from '@hooks/use_tabs';
 import SecurityManager from '@managers/security_manager';
 import {popTopScreen} from '@screens/navigation';
+import {topInsetShared} from '@utils/top_inset_shared';
 
 import ThreadsList from './threads_list';
 import Header from './threads_list/header';
@@ -100,7 +101,7 @@ const GlobalThreads = ({componentId, globalThreadsTab, hasUnreads, teamId}: Prop
     }, [serverUrl, tab]);
 
     const contextStyle = useMemo(() => ({
-        top: defaultHeight,
+        top: defaultHeight + topInsetShared.value,
     }), [defaultHeight]);
 
     const onBackPress = useCallback(() => {
