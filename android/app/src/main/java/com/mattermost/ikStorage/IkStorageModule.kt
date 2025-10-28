fd47c56e7e3b0ce277c3402df83a5949fcef6e3c
Boris Trombert <boris.trombert@infomaniak.com>
2025-10-28 09:20:21 +0100

    fix(credentials): refactor to use constant for master key alias in Credentials and IkStorageModule

--- android/app/src/main/java/com/mattermost/ikStorage/IkStorageModule.kt
+++ android/app/src/main/java/com/mattermost/ikStorage/IkStorageModule.kt
@@ -12,7 +12,7 @@ class IkStorageModule(reactContext: ReactApplicationContext) :
     ReactContextBaseJavaModule(reactContext) {
 
     private val prefs by lazy {
-        val masterKey = MasterKey.Builder(reactContext)
+        val masterKey = MasterKey.Builder(reactContext, "ik_master_key")
             .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
             .build()
 
