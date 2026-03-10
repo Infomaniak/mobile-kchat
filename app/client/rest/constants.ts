d8018d030da417d8e3bb73b3ad93c02bac2b89bb
Boris Trombert <boris.trombert@infomaniak.com>
2026-03-10 16:05:30 +0100

    chore: build 1.0.45-rc.1

--- app/client/rest/constants.ts
+++ app/client/rest/constants.ts
@@ -23,7 +23,6 @@ export const PER_PAGE_DEFAULT = 60;
 // export const BASE_SERVER_URL = 'https://kchat.preprod.dev.infomaniak.ch';
 // export const BASE_LOGIN_URL = 'https://login.preprod.dev.infomaniak.ch';
 
-
 // Staging Refonte équipe
 export const BASE_SERVER_URL = 'https://infomaniak.kchat.staging-165797.dev.infomaniak.ch';
 export const BASE_LOGIN_URL = 'https://login.staging-165797.dev.infomaniak.ch';
