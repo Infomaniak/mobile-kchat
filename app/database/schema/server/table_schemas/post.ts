3cc5a98e9a6c37a9bab71385e099f37e446c6f22
Aurelien SBERRO <aurelien.sberro@infomaniak.com>
2026-05-18 12:52:42 +0200

    perf(schema): Add missing indexes to frequently queried columns

--- app/database/schema/server/table_schemas/post.ts
+++ app/database/schema/server/table_schemas/post.ts
@@ -11,8 +11,8 @@ export default tableSchema({
     name: POST,
     columns: [
         {name: 'channel_id', type: 'string', isIndexed: true},
-        {name: 'create_at', type: 'number'},
-        {name: 'delete_at', type: 'number'},
+        {name: 'create_at', type: 'number', isIndexed: true},
+        {name: 'delete_at', type: 'number', isIndexed: true},
         {name: 'edit_at', type: 'number'},
         {name: 'is_pinned', type: 'boolean'},
         {name: 'message', type: 'string'},
