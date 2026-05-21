b674f1bfe3c62d1518b07cf6a6f425c87d9cff82
Boris Trombert <boris.trombert@infomaniak.com>
2026-05-21 13:02:20 +0200

    re add network mock

--- app/utils/url/index.test.ts
+++ app/utils/url/index.test.ts
@@ -27,6 +27,10 @@ import {
     tryOpenURL,
 } from './index';
 
+jest.mock('@mattermost/react-native-network-client', () => ({
+    head: jest.fn(),
+}));
+
 const mockedOpenURL = jest.spyOn(Linking, 'openURL');
 
 describe('safeDecodeURIComponent', () => {
