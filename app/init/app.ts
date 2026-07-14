3167c2e10a167085996b4b6e89a730989944d480
Boris Trombert <boris.trombert@infomaniak.com>
2026-07-14 09:29:33 +0200

    revert: perf log

--- app/init/app.ts
+++ app/init/app.ts
@@ -15,7 +15,6 @@ import {registerScreens} from '@screens/index';
 import {registerNavigationListeners} from '@screens/navigation';
 import EphemeralStore from '@store/ephemeral_store';
 import NavigationStore from '@store/navigation_store';
-import {logInfo} from '@utils/log';
 import {withMinDuration} from '@utils/timing';
 
 // Controls whether the main initialization (database, etc...) is done, either on app launch
@@ -42,61 +41,34 @@ export async function initialize() {
     if (!baseAppInitialized) {
         baseAppInitialized = true;
 
-        let stepStart = Date.now();
         serverCredentials = await getAllServerCredentials();
-        logInfo('[AppInit] getAllServerCredentials took', Date.now() - stepStart, 'ms');
-
         const serverUrls = serverCredentials.map((credential) => credential.serverUrl);
 
-        stepStart = Date.now();
         await DatabaseManager.init(serverUrls);
-        logInfo('[AppInit] DatabaseManager.init took', Date.now() - stepStart, 'ms');
-
-        stepStart = Date.now();
         await NetworkManager.init(serverCredentials);
-        logInfo('[AppInit] NetworkManager.init took', Date.now() - stepStart, 'ms');
-
-        stepStart = Date.now();
         await ImageCacheMigration.init();
-        logInfo('[AppInit] ImageCacheMigration.init took', Date.now() - stepStart, 'ms');
 
-        stepStart = Date.now();
         GlobalEventHandler.init();
-        logInfo('[AppInit] GlobalEventHandler.init took', Date.now() - stepStart, 'ms');
-
-        stepStart = Date.now();
         SessionManager.init();
-        logInfo('[AppInit] SessionManager.init took', Date.now() - stepStart, 'ms');
     }
 }
 
 export async function start() {
-    const totalStart = Date.now();
-
     await withMinDuration(async () => {
         NavigationStore.reset();
         EphemeralStore.setCurrentThreadId('');
         EphemeralStore.setProcessingNotification('');
 
-        let stepStart = Date.now();
         await initialize();
-        logInfo('[AppInit] initialize() total took', Date.now() - stepStart, 'ms');
 
-        stepStart = Date.now();
         PushNotifications.init(serverCredentials.length > 0);
-        logInfo('[AppInit] PushNotifications.init took', Date.now() - stepStart, 'ms');
 
-        stepStart = Date.now();
         registerNavigationListeners();
         registerScreens();
-        logInfo('[AppInit] registerNavigationListeners + registerScreens took', Date.now() - stepStart, 'ms');
 
-        stepStart = Date.now();
         await WebsocketManager.init(serverCredentials);
-        logInfo('[AppInit] WebsocketManager.init took', Date.now() - stepStart, 'ms');
-    }, 1000); // Ik: min duration for splashscreen
 
-    logInfo('[AppInit] TOTAL start() took', Date.now() - totalStart, 'ms');
+    }, 1000); // Ik: min duration for splashscreen
 
     if (!__DEV__) {
         // Ik Analytics / Matomo
