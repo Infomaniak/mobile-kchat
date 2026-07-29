ce008036611c7b8e510f9f02d9fd72170865db83
Boris Trombert <boris.trombert@infomaniak.com>
2026-07-29 11:06:03 +0200

    refactor: rename Screen constants to snake_case for expo-router
    All Screen constants now use snake_case to match expo-router file-based
    routing conventions. Add UNAUTHENTICATED_SCREENS, HOME_TAB_SCREENS,
    MODAL_SCREENS, SCREENS_AS_BOTTOM_SHEET sets for route group classification.
    Rename INFOMANIAK_* to IK_* constants. Add CHANNEL_LIST, GENERIC_BOTTOM_SHEET.

--- app/constants/screens.ts
+++ app/constants/screens.ts
@@ -3,107 +3,101 @@
 
 import {AGENTS_REWRITE_OPTIONS} from '@agents/constants/screens';
 
-export const ABOUT = 'About';
-export const ACCOUNT = 'Account';
-export const AI_OPTIONS = 'AiOptions';
-export const APPS_FORM = 'AppForm';
-export const ATTACHMENT_OPTIONS = 'AttachmentOptions';
-export const BOTTOM_SHEET = 'BottomSheet';
-export const BROWSE_CHANNELS = 'BrowseChannels';
-export const CALL = 'Call';
-export const CALL_PARTICIPANTS = 'CallParticipants';
-export const CALL_HOST_CONTROLS = 'CallHostControls';
-export const CHANNEL = 'Channel';
-export const CHANNEL_ADD_MEMBERS = 'ChannelAddMembers';
-export const CHANNEL_BANNER = 'ChannelBanner';
-export const CHANNEL_FILES = 'ChannelFiles';
-export const CHANNEL_INFO = 'ChannelInfo';
-export const CHANNEL_NOTIFICATION_PREFERENCES = 'ChannelNotificationPreferences';
-export const CHANNEL_SETTINGS = 'ChannelSettings';
-export const CODE = 'Code';
-export const CONVERT_GM_TO_CHANNEL = 'ConvertGMToChannel';
-export const CREATE_DIRECT_MESSAGE = 'CreateDirectMessage';
-export const CREATE_OR_EDIT_CHANNEL = 'CreateOrEditChannel';
-export const CREATE_TEAM = 'CreateTeam';
-export const COMPONENT_LIBRARY = 'ComponentLibrary';
-export const CUSTOM_STATUS = 'CustomStatus';
-export const CUSTOM_STATUS_CLEAR_AFTER = 'CustomStatusClearAfter';
-export const DRAFT = 'Draft';
-export const DRAFT_SCHEDULED_POST_OPTIONS = 'DraftScheduledPostOptions';
-export const EDIT_POST = 'EditPost';
-export const EDIT_PROFILE = 'EditProfile';
-export const EDIT_SERVER = 'EditServer';
-export const EMOJI_PICKER = 'EmojiPicker';
-export const FIND_CHANNELS = 'FindChannels';
-export const FORGOT_PASSWORD = 'ForgotPassword';
-export const GALLERY = 'Gallery';
-export const GENERIC_OVERLAY = 'GenericOverlay';
-export const GLOBAL_DRAFTS = 'GlobalDrafts';
-export const GLOBAL_DRAFTS_AND_SCHEDULED_POSTS = 'GlobalDraftsAndScheduledPosts';
-export const GLOBAL_THREADS = 'GlobalThreads';
-export const GROUP_MEMBERS = 'GroupMembers';
-export const HOME = 'Home';
-export const INTEGRATION_SELECTOR = 'IntegrationSelector';
-export const INTERACTIVE_DIALOG = 'InteractiveDialog';
-export const DIALOG_ROUTER = 'DialogRouter';
-export const INVITE = 'Invite';
-export const IN_APP_NOTIFICATION = 'InAppNotification';
-export const JOIN_TEAM = 'JoinTeam';
-export const LEAVE_CHANNEL_MEMBERS = 'LeaveChannelModal';
-export const LATEX = 'Latex';
-export const LOGIN = 'Login';
-export const MANAGE_CHANNEL_MEMBERS = 'ManageChannelMembers';
-export const MENTIONS = 'Mentions';
-export const MFA = 'MFA';
-export const ONBOARDING = 'Onboarding';
-export const PDF_VIEWER = 'PdfViewer';
-export const PERMALINK = 'Permalink';
-export const PINNED_MESSAGES = 'PinnedMessages';
-export const POST_OPTIONS = 'PostOptions';
-export const POST_PRIORITY_PICKER = 'PostPriorityPicker';
-export const REACTIONS = 'Reactions';
-export const REPORT_PROBLEM = 'ReportProblem';
-export const RESCHEDULE_DRAFT = 'RescheduleDraft';
-export const REVIEW_APP = 'ReviewApp';
-export const SAVED_MESSAGES = 'SavedMessages';
-export const SCHEDULED_POST_OPTIONS = 'ScheduledPostOptions';
-export const SEARCH = 'Search';
-export const SELECT_TEAM = 'SelectTeam';
-export const SEND_FEEDBACK = 'SendFeedback';
-export const FEEDBACK_OPTIONS = 'FeedbackOptions';
-export const SERVER = 'Server';
-export const SETTINGS = 'Settings';
-export const SETTINGS_ADVANCED = 'SettingsAdvanced';
-export const SETTINGS_DISPLAY = 'SettingsDisplay';
-export const SETTINGS_DISPLAY_CLOCK = 'SettingsDisplayClock';
-export const SETTINGS_DISPLAY_CRT = 'SettingsDisplayCRT';
-export const SETTINGS_DISPLAY_THEME = 'SettingsDisplayTheme';
-export const SETTINGS_DISPLAY_TIMEZONE = 'SettingsDisplayTimezone';
-export const SETTINGS_DISPLAY_TIMEZONE_SELECT = 'SettingsDisplayTimezoneSelect';
-export const SETTINGS_NOTIFICATION = 'SettingsNotification';
-export const SETTINGS_NOTIFICATION_AUTO_RESPONDER = 'SettingsNotificationAutoResponder';
-export const SETTINGS_NOTIFICATION_EMAIL = 'SettingsNotificationEmail';
-export const SETTINGS_NOTIFICATION_MENTION = 'SettingsNotificationMention';
-export const SETTINGS_NOTIFICATION_PUSH = 'SettingsNotificationPush';
-export const SETTINGS_NOTIFICATION_CALL = 'SettingsNotificationCall';
-export const SHARE_FEEDBACK = 'ShareFeedback';
-export const SNACK_BAR = 'SnackBar';
-export const TABLE = 'Table';
-export const TEAM_SELECTOR_LIST = 'TeamSelectorList';
-export const TERMS_OF_SERVICE = 'TermsOfService';
-export const THREAD = 'Thread';
-export const THREAD_FOLLOW_BUTTON = 'ThreadFollowButton';
-export const THREAD_OPTIONS = 'ThreadOptions';
-export const USER_PROFILE = 'UserProfile';
-export const INFOMANIAK_LOGIN = 'InfomaniakLogin';
-export const INFOMANIAK_NO_TEAMS = 'InfomaniakNoTeams';
-export const INFOMANIAK_QUOTA_EXCEEDED = 'InfomaniakQuotaExceeded';
-
-export const INFOMANIAK_REMINDER = 'InfomaniakReminder';
-export const INFOMANIAK_EVOLVE = 'InfomaniakEvolve';
-export const TRANSCRIPTION = 'Transcription';
-export const DEBUG_PERFORMANCE = 'DebugPerformance';
-export const SHOW_TRANSLATION = 'ShowTranslation';
+const ABOUT = 'about';
+const ACCOUNT = 'account';
+const AI_OPTIONS = 'ai_options';
+const APPS_FORM = 'apps_form';
+const ATTACHMENT_OPTIONS = 'attachment_options';
+const BOTTOM_SHEET = '(bottom_sheet)';
+const GENERIC_BOTTOM_SHEET = 'generic_bottom_sheet';
+const BROWSE_CHANNELS = 'browse_channels';
+const CALL = 'call';
+const CALL_PARTICIPANTS = 'call_participants';
+const CALL_HOST_CONTROLS = 'call_host_controls';
+const CHANNEL = 'channel';
+const CHANNEL_ADD_MEMBERS = 'channel_add_members';
+const CHANNEL_FILES = 'channel_files';
+const CHANNEL_INFO = '(channel_info)';
+const CHANNEL_LIST = 'channel_list';
+const CHANNEL_NOTIFICATION_PREFERENCES = 'channel_notification_preferences';
+const CHANNEL_SETTINGS = 'channel_settings';
+const CODE = 'code';
+const CONVERT_GM_TO_CHANNEL = 'convert_gm_to_channel';
+const CREATE_DIRECT_MESSAGE = 'create_direct_message';
+const CREATE_OR_EDIT_CHANNEL = 'create_or_edit_channel';
+const COMPONENT_LIBRARY = 'component_library';
+const CUSTOM_STATUS = '(custom_status)';
+const CUSTOM_STATUS_CLEAR_AFTER = 'custom_status_clear_after';
+const DIALOG_ROUTER = 'dialog_router';
+const DRAFT_SCHEDULED_POST_OPTIONS = 'draft_scheduled_post_options';
+const EDIT_POST = 'edit_post';
+const EDIT_PROFILE = 'edit_profile';
+const EDIT_SERVER = 'edit_server';
+const EMOJI_PICKER = 'emoji_picker';
+const FEEDBACK_OPTIONS = 'feedback_options';
+const FIND_CHANNELS = 'find_channels';
+const FORGOT_PASSWORD = 'forgot_password';
+const GALLERY = 'gallery';
+const GLOBAL_DRAFTS = 'global_drafts';
+const GLOBAL_THREADS = 'global_threads';
+const GROUP_MEMBERS = 'group_members';
+const HOME = '(home)';
+const IK_EVOLVE = 'ik_evolve';
+const IK_LOGIN = 'ik_login';
+const IK_NO_TEAMS = 'ik_no_teams';
+const IK_QUOTA_EXCEEDED = 'ik_quota_exceeded';
+const IK_REMINDER = 'ik_reminder';
+const INTEGRATION_SELECTOR = 'integration_selector';
+const INTERACTIVE_DIALOG = 'interactive_dialog';
+const INVITE = 'invite';
+const IN_APP_NOTIFICATION = 'in_app_notification';
+const JOIN_TEAM = 'join_team';
+const LATEX = 'latex';
+const LOGIN = 'login';
+const MANAGE_CHANNEL_MEMBERS = 'manage_channel_members';
+const MENTIONS = 'mentions';
+const MFA = 'mfa';
+const ONBOARDING = 'onboarding';
+const PDF_VIEWER = 'pdf_viewer';
+const PERMALINK = 'permalink';
+const PINNED_MESSAGES = 'pinned_messages';
+const POST_OPTIONS = 'post_options';
+const POST_PRIORITY_PICKER = 'post_priority_picker';
+const REACTIONS = 'reactions';
+const REPORT_PROBLEM = 'report_problem';
+const RESCHEDULE_DRAFT = 'reschedule_draft';
+const REVIEW_APP = 'review_app';
+const SAVED_MESSAGES = 'saved_messages';
+const SCHEDULED_POST_OPTIONS = 'scheduled_post_options';
+const SEARCH = 'search';
+const SELECT_TEAM = 'select_team';
+const SEND_FEEDBACK = 'send_feedback';
+const SERVER = 'server';
+const SETTINGS = '(settings)';
+const SETTINGS_ADVANCED = 'settings_advanced';
+const SETTINGS_DISPLAY = 'settings_display';
+const SETTINGS_DISPLAY_CLOCK = 'settings_display_clock';
+const SETTINGS_DISPLAY_CRT = 'settings_display_crt';
+const SETTINGS_DISPLAY_THEME = 'settings_display_theme';
+const SETTINGS_DISPLAY_TIMEZONE = 'settings_display_timezone';
+const SETTINGS_DISPLAY_TIMEZONE_SELECT = 'settings_display_timezone_select';
+const SETTINGS_NOTIFICATION = 'settings_notification';
+const SETTINGS_NOTIFICATION_AUTO_RESPONDER = 'settings_notification_auto_responder';
+const SETTINGS_NOTIFICATION_EMAIL = 'settings_notification_email';
+const SETTINGS_NOTIFICATION_MENTION = 'settings_notification_mention';
+const SETTINGS_NOTIFICATION_PUSH = 'settings_notification_push';
+const SETTINGS_NOTIFICATION_CALL = 'settings_notification_call';
+const SHARE_FEEDBACK = 'share_feedback';
+const SNACK_BAR = 'snack_bar';
+const TABLE = 'table';
+const TEAM_SELECTOR_LIST = 'team_selector_list';
+const TERMS_OF_SERVICE = 'terms_of_service';
+const THREAD = 'thread';
+const THREAD_FOLLOW_BUTTON = 'thread_follow_button';
+const THREAD_OPTIONS = 'thread_options';
+const USER_PROFILE = 'user_profile';
+const DEBUG_PERFORMANCE = 'debug_performance';
+const SHOW_TRANSLATION = 'show_translation';
 
 export default {
     ABOUT,
@@ -112,15 +106,16 @@ export default {
     APPS_FORM,
     ATTACHMENT_OPTIONS,
     BOTTOM_SHEET,
+    GENERIC_BOTTOM_SHEET,
     BROWSE_CHANNELS,
     CALL,
     CALL_PARTICIPANTS,
     CALL_HOST_CONTROLS,
     CHANNEL,
     CHANNEL_ADD_MEMBERS,
-    CHANNEL_BANNER,
     CHANNEL_FILES,
     CHANNEL_INFO,
+    CHANNEL_LIST,
     CHANNEL_NOTIFICATION_PREFERENCES,
     CHANNEL_SETTINGS,
     CODE,
@@ -128,30 +123,32 @@ export default {
     COMPONENT_LIBRARY,
     CREATE_DIRECT_MESSAGE,
     CREATE_OR_EDIT_CHANNEL,
-    CREATE_TEAM,
     CUSTOM_STATUS,
     CUSTOM_STATUS_CLEAR_AFTER,
+    DIALOG_ROUTER,
     DRAFT_SCHEDULED_POST_OPTIONS,
     EDIT_POST,
     EDIT_PROFILE,
     EDIT_SERVER,
     EMOJI_PICKER,
+    FEEDBACK_OPTIONS,
     FIND_CHANNELS,
     FORGOT_PASSWORD,
     GALLERY,
-    GENERIC_OVERLAY,
     GLOBAL_DRAFTS,
-    GLOBAL_DRAFTS_AND_SCHEDULED_POSTS,
     GLOBAL_THREADS,
     GROUP_MEMBERS,
     HOME,
+    IK_EVOLVE,
+    IK_LOGIN,
+    IK_NO_TEAMS,
+    IK_QUOTA_EXCEEDED,
+    IK_REMINDER,
     INTEGRATION_SELECTOR,
     INTERACTIVE_DIALOG,
-    DIALOG_ROUTER,
     INVITE,
     IN_APP_NOTIFICATION,
     JOIN_TEAM,
-    LEAVE_CHANNEL_MEMBERS,
     LATEX,
     LOGIN,
     MANAGE_CHANNEL_MEMBERS,
@@ -172,7 +169,6 @@ export default {
     SEARCH,
     SELECT_TEAM,
     SEND_FEEDBACK,
-    FEEDBACK_OPTIONS,
     SERVER,
     SETTINGS,
     SETTINGS_ADVANCED,
@@ -197,64 +193,80 @@ export default {
     THREAD_FOLLOW_BUTTON,
     THREAD_OPTIONS,
     USER_PROFILE,
-    INFOMANIAK_LOGIN,
-    INFOMANIAK_NO_TEAMS,
-    INFOMANIAK_QUOTA_EXCEEDED,
-    INFOMANIAK_REMINDER,
-    INFOMANIAK_EVOLVE,
-    TRANSCRIPTION,
     DEBUG_PERFORMANCE,
     SHOW_TRANSLATION,
     AGENTS_REWRITE_OPTIONS,
 } as const;
 
-export const MODAL_SCREENS_WITHOUT_BACK = new Set<string>([
+export const MODAL_SCREENS = new Set<string>([
     BROWSE_CHANNELS,
     CHANNEL_INFO,
     CHANNEL_ADD_MEMBERS,
+    CREATE_OR_EDIT_CHANNEL,
     CREATE_DIRECT_MESSAGE,
-    CREATE_TEAM,
     CUSTOM_STATUS,
+    DIALOG_ROUTER,
     EDIT_POST,
     EDIT_PROFILE,
     EDIT_SERVER,
     FIND_CHANNELS,
+    FEEDBACK_OPTIONS,
     GALLERY,
+    INTEGRATION_SELECTOR,
     INVITE,
+    JOIN_TEAM,
     MANAGE_CHANNEL_MEMBERS,
     PDF_VIEWER,
-    PERMALINK,
+    REPORT_PROBLEM,
     RESCHEDULE_DRAFT,
+    SEND_FEEDBACK,
+    SETTINGS,
+]);
+
+export const SCREENS_AS_BOTTOM_SHEET = new Set<string>([
+    GENERIC_BOTTOM_SHEET,
+    ATTACHMENT_OPTIONS,
+    CALL_PARTICIPANTS,
+    CALL_HOST_CONTROLS,
+    DRAFT_SCHEDULED_POST_OPTIONS,
+    EMOJI_PICKER,
+    POST_OPTIONS,
+    POST_PRIORITY_PICKER,
+    REACTIONS,
+    SCHEDULED_POST_OPTIONS,
+    TERMS_OF_SERVICE,
+    THREAD_OPTIONS,
+    USER_PROFILE,
+    GROUP_MEMBERS,
+    IK_QUOTA_EXCEEDED,
+    IK_REMINDER,
+    AGENTS_REWRITE_OPTIONS,
+]);
+
+export const UNAUTHENTICATED_SCREENS = new Set<string>([
+    ONBOARDING,
+    SERVER,
+    LOGIN,
+    MFA,
+    FORGOT_PASSWORD,
+    IK_LOGIN,
+    IK_NO_TEAMS,
+]);
+
+export const HOME_TAB_SCREENS = new Set<string>([
+    CHANNEL_LIST,
+    SEARCH,
+    MENTIONS,
+    SAVED_MESSAGES,
+    ACCOUNT,
 ]);
 
 export const SCREENS_WITH_TRANSPARENT_BACKGROUND = new Set<string>([
     PERMALINK,
     REVIEW_APP,
     SNACK_BAR,
-    GENERIC_OVERLAY,
-]);
-
-export const SCREENS_AS_BOTTOM_SHEET = new Set<string>([
-    AGENTS_REWRITE_OPTIONS,
-    ATTACHMENT_OPTIONS,
-    BOTTOM_SHEET,
-    DRAFT_SCHEDULED_POST_OPTIONS,
-    EMOJI_PICKER,
-    POST_OPTIONS,
-    POST_PRIORITY_PICKER,
-    THREAD_OPTIONS,
-    REACTIONS,
-    USER_PROFILE,
-    CALL_PARTICIPANTS,
-    CALL_HOST_CONTROLS,
-    INFOMANIAK_QUOTA_EXCEEDED,
-    INFOMANIAK_REMINDER,
-    SCHEDULED_POST_OPTIONS,
-    GROUP_MEMBERS,
 ]);
 
 export const SCREENS_WITH_EXTRA_KEYBOARD = new Set<string>([CHANNEL, THREAD]);
 
-export const NOT_READY = [
-    CREATE_TEAM,
-];
+export const NOT_READY: string[] = [];
